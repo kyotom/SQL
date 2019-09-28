91ae76c5772a8a5f1a2d5d93a1864e12bad2f1de
Tom <ddd@gmail.com>
2019-09-28 23:05:49 +0900

    コメントを追加

--- gistfile1.sql
+++ gistfile1.sql
@@ -5,6 +5,7 @@ With table_tmp AS (
         , e.name
         , s.due_date
         , e.id AS e_id
+        --締切日以降、スケジュール未定のイベントのみを表示対象とする。
         , CASE WHEN s.due_date >= (SELECT MAX(sysdate) FROM sysdate_dummy) OR due_date IS NULL THEN 1 ELSE 0 END AS flg_output
     FROM
         events e
@@ -25,6 +26,7 @@ FROM (
         , name
         , s_id AS schedule_id
         , due_date
+        --スケジュールが複数あるイベントはシステム日付に最も近い締め切り日を算出する。
         , MIN(due_date) OVER (PARTITION BY e_id) AS min_duedate
     FROM
         table_tmp
@@ -33,5 +35,7 @@ FROM (
 ) a
 WHERE
     (min_duedate = due_date OR min_duedate IS NULL)
+--締切日順に並び替える、ただしスケジュール未定であれば一番下に表示する。
+--締切日が同じであれば、イベントID順に並び替える。
 ORDER BY due_date   NULLS LAST , id
 ;
